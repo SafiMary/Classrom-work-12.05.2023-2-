9df578a3b1570b58d097d3ff6b5e8fdc829398e7
Мария Сафи <safim@yandex.ru>
2023-05-14 13:43:55 +0500

    Add project files.

---
+++ Classrom work 12.05.2023(2)/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Classrom_work_12._05._2023_2_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "";
+            if (args.Length > 0)
+            {
+                path = args[0]; 
+            }
+           
+            Console.WriteLine("Введите путь к файлу или его название: ");
+            path = Console.ReadLine(); //тут будет "test.txt"
+            bool fileExist = File.Exists(path);
+            if (fileExist)
+            {
+                Console.WriteLine("Файл найден.");
+            }
+            else
+            {
+                Console.WriteLine("Файл не найден. Вам необходимо зайти в папку с файлами проекта и проверить наличие файла. В случае его отсутствия, создать его");
+            }
+           
+        }
+    }
+}
